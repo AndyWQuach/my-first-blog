02c2bd47830a297934e7802d077b4f41be534c71
Andy Quach <andyquach@outlook.com>
2019-12-08 13:23:30 +1100

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\andy.quach\documents\03 coding\03 web development\01_appportfolio\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
